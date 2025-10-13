e5943c169e1795edd70bbecd0c29415b9f0dd8ac
renaldi-azis <arifrenaldi0720@gmail.com>
2025-10-13 23:28:01 +0700

    feat: set up backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/images/logo.png'
-import viteLogo from '/vite.svg'
+import React, {useEffect} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
-import LandingPage from './pages'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+ 
   return (
     <>
-      <LandingPage/>
     </>
   )
 }
