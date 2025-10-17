80939ff49248d59402d14a41d93fab336135c07a
renaldi-azis <arifrenaldi0720@gmail.com>
2025-10-17 23:18:53 +0700

    fix: fix directories

--- backend/src/index.js
+++ backend/src/index.js
@@ -41,12 +41,8 @@ if (process.env.NODE_ENV === "production") {
 
 app.get("/api/directories", async (req, res) => {
   const targetPath = req.query.path
-    ? path.resolve(
-        path.dirname(new URL(import.meta.url).pathname),
-        "../",
-        req.query.path
-      )
-    : path.resolve(path.dirname(new URL(import.meta.url).pathname), "../");
+    ? path.resolve(path.dirname(process.cwd()), "../../", req.query.path)
+    : path.resolve(path.dirname(process.cwd()), "../../");
 
   try {
     async function getDirStructure(dirPath, basePath = dirPath) {
