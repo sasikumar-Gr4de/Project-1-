351aa600441f3ca9dbd5f8e27645c27a55e95ff4
renaldi-azis <arifrenaldi0720@gmail.com>
2025-10-17 21:04:30 +0700

    feat: add vercel config

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,6 +28,14 @@ app.use((req, res, next) => {
   next();
 });
 
+if (process.env.NODE_ENV === "production") {
+  const frontendPath = path.join(__dirname, "../../../frontend/dist");
+  app.use(express.static(frontendPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
+
 // Routes
 app.use("/api/auth", authRoutes);
 
