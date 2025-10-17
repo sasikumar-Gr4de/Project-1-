80adda0ba79ecaa36642c19745baeb2900e6dcf9
renaldi-azis <arifrenaldi0720@gmail.com>
2025-10-17 23:01:52 +0700

    fix: fix fs import

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
+import fs from "fs";
 import { fileURLToPath } from "url";
 import { testSupabaseConnection } from "./config/supabase.js";
 
