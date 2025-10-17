9793b0c9b6e1a424a718691a4ad454b7c521a517
renaldi-azis <arifrenaldi0720@gmail.com>
2025-10-17 21:59:41 +0700

    fix: fix production

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../../../frontend");
+  const frontendPath = path.join(__dirname, "frontend/dist");
   app.use(express.static(frontendPath));
   app.get(/.*/, (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
