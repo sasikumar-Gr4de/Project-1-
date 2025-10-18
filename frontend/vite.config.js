7b1daaa8de807da19e57649227b0bcb6ea629dcc
renaldi-azis <arifrenaldi0720@gmail.com>
2025-10-18 20:49:20 +0700

    fix: config vite url api

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwind from "@tailwindcss/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
   resolve: {
