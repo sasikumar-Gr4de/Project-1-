196a74f3b6a685570030c872bb7b7b574791670a
renaldi-azis <arifrenaldi0720@gmail.com>
2025-10-29 11:50:40 +0700

    fix: fix backend endpoints

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,17 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import authRoutes from "./routes/auth.routes.js";
-import uploadRoutes from "./routes/upload.routes.js";
-import clubsRoutes from "./routes/clubs.routes.js";
-import matchesRoutes from "./routes/matches.routes.js";
-import playersRoutes from "./routes/players.routes.js";
-import awsRoutes from "./routes/aws.routes.js";
-import analysisWorksRoutes from "./routes/analysis-works.routes.js";
-import eventsTempRoutes from "./routes/events-temp.routes.js";
-import matchInfoRoutes from "./routes/match-info.routes.js";
-import usersRoutes from "./routes/users.routes.js";
-
 // Load environment variables from .env file
 dotenv.config();
 
@@ -42,17 +31,6 @@ app.use(
     limit: process.env.MAX_FILE_SIZE,
   })
 );
-
-app.use("/api/auth", authRoutes);
-app.use("/api/upload", uploadRoutes);
-app.use("/api/clubs", clubsRoutes);
-app.use("/api/matches", matchesRoutes);
-app.use("/api/players", playersRoutes);
-app.use("/api/aws", awsRoutes);
-app.use("/api/analysis-works", analysisWorksRoutes);
-app.use("/api/events-temp", eventsTempRoutes);
-app.use("/api/match-info", matchInfoRoutes);
-app.use("/api/users", usersRoutes);
 
 app.get("/api", (req, res) => {
   res.json({ success: true, message: "API is running" });
