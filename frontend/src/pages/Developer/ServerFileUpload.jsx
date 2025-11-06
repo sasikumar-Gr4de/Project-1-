cc595ad4689dbdce44e64dcf292ea445a8f30eee
renaldi-azis <arifrenaldi0720@gmail.com>
2025-11-06 22:41:44 +0700

    fix: fix serverFileUpload

--- frontend/src/pages/Developer/ServerFileUpload.jsx
+++ frontend/src/pages/Developer/ServerFileUpload.jsx
@@ -1,14 +1,21 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import FileUpload from "@/components/common/FileUpload";
 
 const ServerFileUpload = () => {
+  const [uploaedUrl, setUploadedUrl] = useState("");
   const onUploadHandler = (results) => {
     const result = results[0];
     const { url } = result;
-    window.alert(`Uploded Url : ${url}`);
+    setUploadedUrl(url);
+    // window.alert(`Uploded Url : ${url}`);
   };
   return (
     <div className="min-h-screen bg-background text-foreground">
+      <div className="text-3xl font-bold text-center p-6">
+        Server File Upload
+      </div>
+      <div className="">{uploaedUrl}</div>
+      <hr />
       <div className="container mx-auto max-w-6xl m-6">
         <FileUpload
           accept="image/*"
